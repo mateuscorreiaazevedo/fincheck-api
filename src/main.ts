13e414baf92cb27a2f2b4922ed6ec3c00451a798
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-08-20 22:06:12 -0300

    refactor: simplify environment configuration and remove Zod validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app';
-import 'dotenv/config'
-import { envConfig } from './infra/config';
+import {config} from 'dotenv'
+import path from 'node:path'
 
 async function bootstrap() {
+  const envFile = process.env.NODE_ENV === 'development' ? '.env.local' : '.env'
+  config({ path: path.resolve(process.cwd(), envFile) })
+
+  const port = process.env.PORT ? Number(process.env.PORT) : 8080
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(envConfig.PORT);
+  await app.listen(port);
 }
 bootstrap();
